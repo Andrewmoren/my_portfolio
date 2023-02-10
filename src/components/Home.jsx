93ad9de37e2ec67590421186807843100aa50e92
Andrew <andrewmoren93@gmail.com>
2023-02-10 17:54:11 +0300

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import home from "../assets/home.png";
 import play from "../assets/play.png";
+import Navbar from "./Navbar";
 const Home = () => {
   return (
-    <div>
-      <img src={home} alt="" />
-    </div>
+    <Section id="home">
+      <Navbar />
+    </Section>
   );
 };
+
+const Section = styled.section`
+  background-image: url(${home});
+  background-size: cover;
+  min-height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
 
 export default Home;
