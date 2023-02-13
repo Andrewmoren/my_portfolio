a86bf7631e1a21ac60902ebddade0c1250000f96
Andrew <andrewmoren93@gmail.com>
2023-02-13 21:31:12 +0300

    home: services 2

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -49,6 +49,49 @@ const Services = () => {
   );
 };
 
-const Section = styled.section``;
+const Section = styled.section`
+  min-height: 100vh;
+  .services {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: center;
+    height: 100%;
+    margin: 0 14rem;
+    margin-top: 10rem;
+    gap: 5rem;
+    &__service {
+      padding: 2rem;
+      &:nth-of-type(3) {
+        background-color: var(--primary-color);
+        .services__service__title {
+          span {
+            color: white;
+          }
+        }
+        .services__service__description {
+          color: white;
+        }
+      }
+      &__image {
+        margin-bottom: 3rem;
+      }
+      &__title {
+        span {
+          color: var(--primary-color);
+          font-weight: bolder;
+        }
+        h2 {
+          font-size: 3rem;
+          line-height: 2.5rem;
+          margin-bottom: 5rem;
+          color: var(--primary-color);
+        }
+      }
+      &__description {
+        color: var(--primary-color);
+      }
+    }
+  }
+`;
 
 export default Services;
