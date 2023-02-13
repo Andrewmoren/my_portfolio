0da222a78c3f8c517b04fc401cccef11b9627848
Andrew <andrewmoren93@gmail.com>
2023-02-13 21:09:36 +0300

    home: services

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,24 +25,29 @@ const Home = () => {
       <div className="info">
         <div className="grid">
           <div className="col">
-            <strong></strong>
-            <p></p>
+            <strong>Agency</strong>
+            <p>Digital Crafters</p>
           </div>
           <div className="col">
-            <strong></strong>
-            <p></p>
+            <strong>Email</strong>
+            <p>example@gmail.com</p>
           </div>
           <div className="col">
-            <strong></strong>
-            <p></p>
+            <strong>Mobile</strong>
+            <p>+23423423325</p>
           </div>
           <div className="col">
-            <strong></strong>
-            <p></p>
+            <strong>Address</strong>
+            <p>Some Street India</p>
           </div>
           <div className="col">
-            <strong></strong>
-            <p></p>
+            <strong>Services</strong>
+            <p>Corporate identity Full cooding</p>
+            <p>Full cooding</p>
+          </div>
+          <div className="col">
+            <strong>Working Hours</strong>
+            <p>Monday to Friday 08:00</p>
           </div>
         </div>
       </div>
@@ -56,6 +61,46 @@ const Section = styled.section`
   min-height: 100vh;
   background-repeat: no-repeat;
   background-position: center;
+  position: relative;
+  .home {
+    height: 100%;
+    .content {
+      height: 100%;
+      color: white;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 1.2rem;
+      margin-top: 8rem;
+      padding-left: 18rem;
+      width: 60%;
+      .title {
+        h1 {
+          font-size: 5rem;
+          line-height: 5.3rem;
+        }
+      }
+      .subTitle {
+        p {
+          width: 70%;
+          margin-bottom: 2rem;
+        }
+      }
+    }
+  }
+  .info {
+    position: absolute;
+    bottom: -6rem;
+    right: 0;
+    background-color: var(--secondary-color);
+    color: white;
+    padding: 4rem;
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 4rem;
+    }
+  }
 `;
 
 export default Home;
